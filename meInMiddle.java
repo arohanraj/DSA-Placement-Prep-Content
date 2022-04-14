b380e2806f7209b12420a139b50f852363870e29
arohanraj <90061844+arohanraj@users.noreply.github.com>
2022-04-14 12:32:51 +0530

    Updated sig....

--- meInMiddle.java
+++ meInMiddle.java
@@ -1,3 +1,39 @@
+/*
+Me in middle
+
+Time Limit: 2 sec
+Memory Limit: 128000 kB
+
+Problem Statement
+You are given an array of size n containing distinct integers, where n is odd. Find
+the element which has the same number of lesser elements and the same number
+of greater elements in the array.
+Input
+First line of the input contains an integer, N, which denotes the length of the array.
+Next N inputs are elements of the array.
+Constraints
+1 <= N <= 20
+-10000 <= Arr[i] <= 10000
+Output
+Single integer n which has the same number of lesser elements and the same
+number of greater elements.
+
+Example
+Input:
+3
+3 1 2
+Output:
+2
+Explanation:-
+2 has one greater element 3 and one smaller element 1
+Input:
+5
+2 3 4 9 1
+Output:
+3
+
+*/
+
 import java.util.*;
 
 class meInMiddle{
@@ -20,3 +56,5 @@ class meInMiddle{
         System.out.println(arr[x]);
     }
 }
+
+// This is Contributed by Amit.
